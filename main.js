41c7ec5a2ded88f9ab4dbddd1be15ed9afea4cd4
richi-coder <richicoder@gmail.com>
2023-06-17 16:13:01 -0400

    updating

--- main.js
+++ main.js
@@ -4,28 +4,19 @@ let ownClientID = 'v1S5Sma8reiXZd8lYNzLzoFwpgezPjgDxKb04XR32rk';
 let testClientID = 'gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k';
 
 let button = document.getElementById('fetch-button');
+let container = document.getElementById('container')
+const query = 'mouse pc'
 
-button.addEventListener('click', () => {
+button.addEventListener('click', async() => {
   console.log('button Clicked!');
-  for (let i = 0; i < 5; i++) {
     // USING ANOTHER CLIENT ID since mine is not working ok
-  fetch(`https://api.unsplash.com/photos/random/?query=a banana&client_id=${testClientID}`)
-      .then(res => res.json())
-      .then(res => {
-        let image = document.createElement('img');
-        console.log(res.alt_description);
-        image.width = '300';
-        image.height = '300';
-        image.style.objectFit = 'cover';
-        image.src = res.links.download;
-        document.body.appendChild(image)
-      })
-  }
+  const res = await fetch(`https://api.unsplash.com/search/photos/?per_page=5&query=${query}&client_id=${testClientID}`)
+  const data = await res.json()
+  data.results.map(image => {
+    let newImage = document.createElement('img');
+    newImage.src = image.urls.regular;
+    newImage.style.width = '300px';
+    container.appendChild(newImage)
+  })
 })
 
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    VITE
-  </div>
-`
